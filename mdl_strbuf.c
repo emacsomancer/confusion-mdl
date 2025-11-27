e8c584c9506b02003e6dac229a445441a562be0b
Jordan Hubbard <jordanh@nvidia.com>
2025-11-26 21:38:02 -0800

    Merge WASM changes

--- mdl_strbuf.c
+++ mdl_strbuf.c
@@ -16,7 +16,7 @@
 /*    You should have received a copy of the GNU General Public License     */
 /*    along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 /*****************************************************************************/
-#include <gc/gc.h>
+#include "gc_stub.h"
 #include <string.h>
 #include <assert.h>
 #include "mdl_strbuf.h"
